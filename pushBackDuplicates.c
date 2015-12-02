04f2bc6bc834389719e624374866e0750d709603
Jessica <jessicaslaughter@utexas.edu>
2015-12-01 18:28:13 -0600

    changed push back duplicates

--- pushBackDuplicates.c
+++ pushBackDuplicates.c
@@ -1,36 +1,20 @@
-/* Given an ascending sorted array of ints, write an algorithm that pushes all duplicates to the back */
+/* Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length. */
 
-#include <stdio.h>
-
-int main() {
-	int const size = 10;
-	int i, k, current, next, temp, a[size];
-	
-	printf("Enter %d elements in ascending order:\n", size);
-	for (i = 0; i < size; i++) {
-		scanf("%d", &a[i]);
-	}
-
-	printf("Original list:\n");
-	for (i = 0; i < size; i++) {
-		printf("%d\n", a[i]);
-	}
-
-
-	for (i = 0; i < size - 2; i++) {
-		if (a[i] == a[i+1]) {
-			temp = a[i+1];
-			for (k = i + 1; k < size - 1; k++) {
-				a[k] = a[k+1];
-			}
-			a[size-1] = temp;
-		}
-	}
-
-	printf("List with duplicates in back:\n");
-	for (i = 0; i < size; i++) {
-		printf("%d\n", a[i]);
-	}
-
-	return 0;
+int removeDuplicates(int* nums, int numsSize) {
+    int length, i, j;
+    length = numsSize;
+    if (length < 2) {
+        return length;
+    }
+    i = 0;
+    for (j = 1; j < numsSize; j++) {
+        if (nums[i] != nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
+        else {
+            length--;
+        }
+    }
+    return length;
 }
